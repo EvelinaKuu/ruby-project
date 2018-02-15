afae3befeeea9e7aa83e341b6819b6913bdbcd23
EvelinaKuu <evelina.kuusinen@gmail.com>
2018-02-15 18:06:03 +0000

    created content to seed file

--- QuickDoc/db/seeds.rb
+++ QuickDoc/db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Physician.delete_all
+
+d1 = Physician.create({name: "Angela Walker", specialisation: "Obstetrics and gynaecology", location: "Spitalfields Medical Center", gender: "female", summary: "adlsfjalkflkdsjflajslk"})
+d2 = Physician.create({name: "Joanna Shaffer", specialisation: "Psychiatry", location: "Blackfriars Medical Practice", gender: "female", summary: "adlsfjalkflkdsjflajslk"})
+d3 = Physician.create({name: "Dan Brown", specialisation: "Rheumatology", location: "Blackfriars Medical Practice", gender: "male", summary: "adlsfjalkflkdsjflajslk"})
+d4 = Physician.create({name: "Mathias Little", specialisation: "Psychiatry", location: "Whitechapel Health Centre", gender: "male", summary: "adlsfjalkflkdsjflajslk"})
+d5 = Physician.create({name: "Thalia Cobb", specialisation: "Paediatrics", location: "Spitalfields Medical Center", gender: "female", summary: "adlsfjalkflkdsjflajslk"})
+d6 = Physician.create({name: "Abdullah Lang", specialisation: "Obstetrics and gynaecology", location: "Whitechapel Health Centre", gender: "male", summary: "adlsfjalkflkdsjflajslk"})
